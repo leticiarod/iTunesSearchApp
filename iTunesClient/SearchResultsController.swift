2a195682d5edfca342ec73716a6f62709a7650b8
leticiarod <leticiamrgs@gmail.com>
2017-07-15 01:09:32 -0300

    Model files created (Song, Artist, Album, Genre and Stub Data)

---
+++ iTunesClient/SearchResultsController.swift
@@ -0,0 +1,44 @@
+//
+//  SearchResultsController.swift
+//  iTunesClient
+//
+//  Created by Leticia Rodriguez on 7/15/17.
+//  Copyright © 2017 Leticia Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsController: UITableViewController {
+
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(SearchResultsController.dismissSearchResultsController))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    func dismissSearchResultsController(){
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    
+
+}
---
+++ iTunesClient/Song.swift
@@ -0,0 +1,18 @@
+//
+//  Song.swift
+//  iTunesClient
+//
+//  Created by Leticia Rodriguez on 7/15/17.
+//  Copyright © 2017 Leticia Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+struct Song {
+    let id: Int
+    let name: String
+    let censoredName: String
+    let trackTime: Int
+    let isExplicit: Bool
+    
+}
